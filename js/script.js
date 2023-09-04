d325dc1d81eee38bf26aa32560889300cb58b323
Keshav Narang <mrkeshavnarang@gmail.com>
2023-09-03 20:53:49 -0700

    fixing features

--- js/script.js
+++ js/script.js
@@ -7,14 +7,15 @@ document.addEventListener('DOMContentLoaded', function () {
             
             data.forEach(bill => {
                 const card = document.createElement('div');
-                card.classList.add('col-md-4', 'mb-3');
+                card.classList.add('card', 'mb-3');
+                card.style.border = '2px solid #ccc';
+                card.style.boxShadow = '3px 3px 5px #888';
                 card.innerHTML = `
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">${bill.title}</h5>
-                            <p class="card-text">${bill.summary}</p>
-                            <a href="public_comments.html?id=${bill.bill_id}" class="btn btn-primary">View Comment</a>
-                        </div>
+                    <div class="card-body">
+                        <h6 class="card-subtitle mb-2 text-muted text-center">${bill.bill_id}</h6>
+                        <h5 class="card-title text-center">${bill.title}</h5>
+                        <p class="card-text">Summary: ${bill.summary}</p>
+                        <a href="public_comments.html?id=${bill.bill_id}" class="btn btn-primary d-block mx-auto">Make a Comment</a>
                     </div>
                 `;
                 billList.appendChild(card);
