aa3ebb2fef0ff0de05b6f56d703719cd2eca94ba
Keshav Narang <mrkeshavnarang@gmail.com>
2023-11-05 09:22:25 -0800

    try to show message attempt 5

--- load_bills.js
+++ load_bills.js
@@ -4,15 +4,21 @@ function showComments(interest, billId) {
     fetch(commentsURL)
         .then(response => response.json())
         .then(data => {
-            if (typeof data === 'object' && data !== null) {
-                const commentText = data[billId];
-                if (commentText) {
-                    const editableField = document.createElement('textarea');
-                    editableField.value = commentText;
-                    const modal = document.createElement('div');
-                    modal.appendChild(editableField);
-                    // Append the modal to the document or display it as needed
-                    // ...
+            if (commentText) {
+                    // Create a container to display the comment
+                    const commentContainer = document.createElement('div');
+                    commentContainer.classList.add('comment-container');
+
+                    // Create a paragraph to display the comment text
+                    const commentParagraph = document.createElement('p');
+                    commentParagraph.textContent = commentText;
+
+                    // Append the comment paragraph to the container
+                    commentContainer.appendChild(commentParagraph);
+
+                    // Insert the comment container above the "Make a Comment" button
+                    const makeCommentButton = document.querySelector(`[data-bill-id="${billId}"]`);
+                    makeCommentButton.parentNode.insertBefore(commentContainer, makeCommentButton);
                 } else {
                     console.error(`No comment found for bill ID ${billId} in ${interest}_comments.json`);
                 }
@@ -53,7 +59,11 @@ document.addEventListener("DOMContentLoaded", () => {
                                     <p>Summary: ${bill.title}</p>
                                     <p>Full Text: <a href="${bill.full_text}" target="_blank">${bill.full_text}</a></p>
                                     <p class="card-text">Source: ${sources.join(', ')}</p>
-                                    <a href="javascript:void(0);" class="btn btn-primary d-block mx-auto" onclick="showComments('${interest}', '${bill.bill_id}')">Make a Comment</a>
+                                    <a href="javascript:void(0);" class="btn btn-primary d-block mx-auto" 
+                                        data-bill-id="${bill.bill_id}" 
+                                        onclick="showComments('${interest}', '${bill.bill_id}')">
+                                        Make a Comment
+                                    </a>
                                 </div>
                             `;
                             return { card, sources };
