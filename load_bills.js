d6ec4aeee1d9d8ec8808f2137c183a43eceae2e1
Keshav Narang <mrkeshavnarang@gmail.com>
2023-11-09 15:03:27 -0800

    increase size of bill ID, clarify where it's from,  and error screen for no bills

--- load_bills.js
+++ load_bills.js
@@ -20,23 +20,25 @@ document.addEventListener("DOMContentLoaded", () => {
                             card.style.border = '2px solid #ccc';
                             card.style.boxShadow = '3px 3px 5px #888';
                             card.innerHTML = `
-                                <div class="card-body">
-                                    <h6 class="card-subtitle mb-2 text-muted text-center">${bill.bill_id}</h6>
-                                    <h5 class="card-title text-center">${bill.short_title}</h5>
-                                    <p>Summary: ${bill.title}</p>
-                                    <p>Full Text: <a href="${bill.full_text}" target="_blank">${bill.full_text}</a></p>
-                                    <p class="card-text">Source: ${sources.join(', ')}</p>
-                                    <button class="btn btn-primary d-block mx-auto" 
-                                        data-bill-id="${bill.bill_id}" 
-                                        onclick="toggleComment(this, '${interest}', '${bill.bill_id}')">
-                                        Make a Comment
-                                    </button>
-                                    <div class="comment-container" style="display: none;">
-                                        <textarea style="width: 100%; height: 100px;"></textarea>
-                                        <button style="display: none;">Send an Email</button>
-                                    </div>
-                                </div>
-                            `;
+								<div class="card-body">
+									<h6 class="card-subtitle mb-2 text-muted text-center ${getBillIDStyle(bill.bill_id)}">
+										${bill.bill_id}
+									</h6>
+									<h5 class="card-title text-center">${bill.short_title}</h5>
+									<p>Summary: ${bill.title}</p>
+									<p>Full Text: <a href="${bill.full_text}" target="_blank">${bill.full_text}</a></p>
+									<p class="card-text">Source: ${sources.join(', ')}</p>
+									<button class="btn btn-primary d-block mx-auto"
+										data-bill-id="${bill.bill_id}"
+										onclick="toggleComment(this, '${interest}', '${bill.bill_id}')">
+										Make a Comment
+									</button>
+									<div class="comment-container" style="display: none;">
+										<textarea style="width: 100%; height: 100px;"></textarea>
+										<button style="display: none;">Send an Email</button>
+									</div>
+								</div>
+							`;
                             return { card, sources };
                         } else {
                             return null;
@@ -52,12 +54,18 @@ document.addEventListener("DOMContentLoaded", () => {
         Promise.all(fetchPromises)
             .then(cardsArrays => {
                 const allCards = cardsArrays.flat().filter(item => item !== null);
-                allCards.forEach(({ card, sources }) => {
-                    // Append the card to the billList
-                    billList.appendChild(card);
-                    // Update the sources for each bill
-                    card.querySelector('.card-text').textContent = `Source: ${sources.join(', ')}`;
-                });
+                if (allCards.length > 0) {
+                    allCards.forEach(({ card, sources }) => {
+                        // Append the card to the billList
+                        billList.appendChild(card);
+                        // Update the sources for each bill
+                        card.querySelector('.card-text').textContent = `Source: ${sources.join(', ')}`;
+                    });
+                } else {
+                    const message = document.createElement('p');
+                    message.textContent = `Sorry, there are no ${selectedInterestsArray.length === 1 ? 'bill' : 'bills'} for the selected topic${selectedInterestsArray.length === 1 ? '' : 's'} this time`;
+                    billList.appendChild(message);
+                }
             })
             .catch(error => {
                 console.error("Error handling fetched data:", error);
@@ -106,4 +114,14 @@ function showComments(interest, billId) {
         .catch(error => {
             console.error(`Error loading comments from ${interest}_comments.json:`, error);
         });
+}
+
+function getBillIDStyle(billID) {
+    if (billID.startsWith('hr')) {
+        return 'house-rep';
+    } else if (billID.startsWith('s')) {
+        return 'senate';
+    } else {
+        return '';
+    }
 }
