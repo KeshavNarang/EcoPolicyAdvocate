eee13efa50b54ae9a0fcf8a33cf9f08203dc77e5
Keshav Narang <mrkeshavnarang@gmail.com>
2023-11-09 15:09:23 -0800

    trying to fix sources v1

--- load_bills.js
+++ load_bills.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     if (selectedInterestsArray) {
         const billIdSet = new Set();
+        const billSources = {}; // Object to store sources for each bill
 
         const fetchPromises = selectedInterestsArray.map(interest => {
             const dataURL = `bills/${interest}_data.json`;
@@ -13,33 +14,37 @@ document.addEventListener("DOMContentLoaded", () => {
                     return data.map(bill => {
                         if (!billIdSet.has(bill.bill_id)) {
                             billIdSet.add(bill.bill_id);
-                            // Create an array to store the sources
-                            const sources = [interest];
+                            // Update the sources for this bill
+                            if (!billSources[bill.bill_id]) {
+                                billSources[bill.bill_id] = [interest];
+                            } else {
+                                billSources[bill.bill_id].push(interest);
+                            }
+                            // Create a card for the bill
                             const card = document.createElement('div');
                             card.classList.add('card', 'mb-3');
                             card.style.border = '2px solid #ccc';
                             card.style.boxShadow = '3px 3px 5px #888';
                             card.innerHTML = `
-								<div class="card-body">
-									<h6 class="card-subtitle mb-2 text-muted text-center ${getBillIDStyle(bill.bill_id)}">
-										${bill.bill_id}
-									</h6>
-									<h5 class="card-title text-center">${bill.short_title}</h5>
-									<p>Summary: ${bill.title}</p>
-									<p>Full Text: <a href="${bill.full_text}" target="_blank">${bill.full_text}</a></p>
-									<p class="card-text">Source: ${sources.join(', ')}</p>
-									<button class="btn btn-primary d-block mx-auto"
-										data-bill-id="${bill.bill_id}"
-										onclick="toggleComment(this, '${interest}', '${bill.bill_id}')">
-										Make a Comment
-									</button>
-									<div class="comment-container" style="display: none;">
-										<textarea style="width: 100%; height: 100px;"></textarea>
-										<button style="display: none;">Send an Email</button>
-									</div>
-								</div>
-							`;
-                            return { card, sources };
+                                <div class="card-body">
+                                    <h6 class="card-subtitle mb-2 text-muted text-center ${getBillIDStyle(bill.bill_id)}">
+                                        ${bill.bill_id} (source: ${billSources[bill.bill_id].join(', ')})
+                                    </h6>
+                                    <h5 class="card-title text-center">${bill.short_title}</h5>
+                                    <p>Summary: ${bill.title}</p>
+                                    <p>Full Text: <a href="${bill.full_text}" target="_blank">${bill.full_text}</a></p>
+                                    <button class="btn btn-primary d-block mx-auto"
+                                        data-bill-id="${bill.bill_id}"
+                                        onclick="toggleComment(this, '${interest}', '${bill.bill_id}')">
+                                        Make a Comment
+                                    </button>
+                                    <div class="comment-container" style="display: none;">
+                                        <textarea style="width: 100%; height: 100px;"></textarea>
+                                        <button style="display: none;">Send an Email</button>
+                                    </div>
+                                </div>
+                            `;
+                            return { card };
                         } else {
                             return null;
                         }
@@ -55,11 +60,9 @@ document.addEventListener("DOMContentLoaded", () => {
             .then(cardsArrays => {
                 const allCards = cardsArrays.flat().filter(item => item !== null);
                 if (allCards.length > 0) {
-                    allCards.forEach(({ card, sources }) => {
+                    allCards.forEach(({ card }) => {
                         // Append the card to the billList
                         billList.appendChild(card);
-                        // Update the sources for each bill
-                        card.querySelector('.card-text').textContent = `Source: ${sources.join(', ')}`;
                     });
                 } else {
                     const message = document.createElement('p');
